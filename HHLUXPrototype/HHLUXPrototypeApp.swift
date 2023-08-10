638dc0ff77df454720d6af028822684dfbbbf073
Vlad Alexa <vlad.alexa@hedgehoglab.com>
2023-08-10 10:04:01 +0100

    Initial Commit

---
+++ HHLUXPrototype/HHLUXPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHLUXPrototypeApp.swift
+//  HHLUXPrototype
+//
+//  Created by vlad on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HHLUXPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
